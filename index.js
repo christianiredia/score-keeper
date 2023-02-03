faa4918b1a572d9d4942ed8310433fec449c4437
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-02-03 01:20:07 -0600

    Refactored code to make it more dry

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const p1Button = document.querySelector("#p1Button");
-const p2Button = document.querySelector("#p2Button");
-const p1Display = document.querySelector("#p1Display");
-const p2Display = document.querySelector("#p2Display");
+const p1 = {
+  score: 0,
+  button: document.querySelector("#p1Button"),
+  display: document.querySelector("#p1Display"),
+};
+
+const p2 = {
+  score: 0,
+  button: document.querySelector("#p2Button"),
+  display: document.querySelector("#p2Display"),
+};
+
 const resetButton = document.querySelector("#reset");
 const winningScoreSelect = document.querySelector("#playto");
 
@@ -11,48 +19,40 @@ let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-// Button Events
-p1Button.addEventListener("click", function () {
+const updateScores = (player, opponent) => {
   if (!isGameOver) {
-    p1Score++;
-    if (p1Score === winningScore) {
+    player.score++;
+    if (player.score === winningScore) {
       isGameOver = true;
-      p1Display.classList.add("has-text-success");
-      p2Display.classList.add("has-text-danger");
+      player.display.classList.add("has-text-success");
+      opponent.display.classList.add("has-text-danger");
       // Adding Bulma Css event to disable buttons
-      p1Button.disabled = true;
-      p2Button.disabled = true;
+      player.button.disabled = true;
+      opponent.button.disabled = true;
     }
-    p1Display.textContent = p1Score;
+    player.display.textContent = player.score;
   }
+};
+// Button Events
+p1.button.addEventListener("click", function () {
+  updateScores(p1, p2);
 });
 
-p2Button.addEventListener("click", function () {
-  if (!isGameOver) {
-    p2Score++;
-    if (p2Score === winningScore) {
-      isGameOver = true;
-      p2Display.classList.add("has-text-success");
-      p1Display.classList.add("has-text-danger");
-      // Adding Bulma Css event to disable buttons
-      p1Button.disabled = true;
-      p2Button.disabled = true;
-    }
-    p2Display.textContent = p2Score;
-  }
+p2.button.addEventListener("click", function () {
+  updateScores(p2, p1);
 });
 
 const reset = () => {
-  p1Score = 0;
-  p2Score = 0;
+  p1.score = 0;
+  p2.score = 0;
   isGameOver = false;
-  p1Display.textContent = p1Score;
-  p2Display.textContent = p2Score;
-  p1Display.classList.remove("has-text-success", "has-text-danger");
-  p2Display.classList.remove("has-text-success", "has-text-danger");
+  p1.display.textContent = p1.score;
+  p2.display.textContent = p2.score;
+  p1.display.classList.remove("has-text-success", "has-text-danger");
+  p2.display.classList.remove("has-text-success", "has-text-danger");
   //Removing the Bulma disabled feature when game is reset
-  p1Button.disabled = false;
-  p2Button.disabled = false;
+  p1.button.disabled = false;
+  p2.button.disabled = false;
 };
 
 resetButton.addEventListener("click", reset);
@@ -60,5 +60,6 @@ resetButton.addEventListener("click", reset);
 // When value of the winning score changes
 winningScoreSelect.addEventListener("change", function () {
   winningScore = parseInt(this.value);
+  // Reset the game whhen the select value changes
   reset();
 });
