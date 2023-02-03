5e1528a8907d5c6654840aff9e42fe3116398575
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-02-03 00:33:42 -0600

    Reset function resets the score and removes added classes. Added function to update the winning score based off select option user picks

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const p2Button = document.querySelector("#p2Button");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 const resetButton = document.querySelector("#reset");
+const winningScoreSelect = document.querySelector("#playto");
 
 // Scorecard
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 // Button Events
@@ -16,6 +17,8 @@ p1Button.addEventListener("click", function () {
     p1Score++;
     if (p1Score === winningScore) {
       isGameOver = true;
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
     }
     p1Display.textContent = p1Score;
   }
@@ -26,15 +29,27 @@ p2Button.addEventListener("click", function () {
     p2Score++;
     if (p2Score === winningScore) {
       isGameOver = true;
+      p2Display.classList.add("winner");
+      p1Display.classList.add("loser");
     }
     p2Display.textContent = p2Score;
   }
 });
 
-resetButton.addEventListener("click", function () {
+const reset = () => {
   p1Score = 0;
   p2Score = 0;
   isGameOver = false;
   p1Display.textContent = p1Score;
   p2Display.textContent = p2Score;
+  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("winner", "loser");
+};
+
+resetButton.addEventListener("click", reset);
+
+// When value of the winning score changes
+winningScoreSelect.addEventListener("change", function () {
+  winningScore = parseInt(this.value);
+  reset();
 });
